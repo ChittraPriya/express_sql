ddb2df6f45937a1390bbaf4f82d48b0a43c7cbbe
chittrapriya5796 <krcpriya@gmail.com>
2026-03-23 04:30:14 +0530

    CRUD operations added

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const db = require('./db')
+const bodyParser = require('body-parser')
 
 const app = express()
+//parse the body in the request object
+app.use(bodyParser.json());
+
 //server setup
 app.get('/', (req,res) => {
     res.status(200).json({message: 'express with mysql'})
@@ -19,4 +23,49 @@ app.get('/todo', (req,res)=> {
     })
 })
 
+
+//Create a new Todo
+app.post('/todo', (req,res) => {
+    //create an sql statement
+    const {tittle,description,isCompleted} = req.body
+   //prepare the insert statement   const sql = 'insert into todo ( tittle, description,isCompleted) values (?, ? ,?)';
+   //perform the query
+   db.query(sql, [tittle, description, isCompleted], (err, result) => {
+    if(err){
+        return res.status(500).json({message: 'Error Fetching Details'})
+    }
+    res.status(201).json({message: 'new todo created', todo:result})
+   })
+})
+
+//Update a new Todo
+app.put('/todo/:id', (req,res) =>{
+    const {id} = req.params
+     //create an sql statement
+    const {tittle,description,isCompleted} = req.body
+    //update the insert statement
+   const sql = 'update todo set tittle = ? , description = ?, isCompleted= ? where id = ?';
+   //perform the query
+   db.query(sql, [tittle, description, isCompleted, id], (err, result) => {
+    if(err){
+        return res.status(500).json({message: 'Error Fetching Details'})
+    }
+    res.status(200).json({message: 'todo update successfully', todo:result})
+   })
+})
+
+   //Delete todo
+   app.delete('/todo/:id', (req,res)=>{
+    const {id} = req.params;
+    //create a sql statement
+    const sql = 'delete from todo where id = ?';
+    //perform the query
+    db.query(sql, [id], (err, result) => {
+        if(err){
+            return res.status(500).json({message: 'Error Fetching Details'})
+        }
+        res.status(200).json({ message: 'Deleted SuccessFully', todo: result})
+    })
+   })
+   
 module.exports = app
